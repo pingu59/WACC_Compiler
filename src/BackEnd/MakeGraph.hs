2488c0be825bd29e6de6dbed7bf886f7649c890b
roro47 <qianyi.shu17@imperial.ac.uk>
2019-02-21 11:49:57 +0000

    add comments on instrsToGraph

--- src/BackEnd/MakeGraph.hs
+++ src/BackEnd/MakeGraph.hs
@@ -7,6 +7,11 @@ import BackEnd.Assem as Assem
 
 
 -- transform instructions to control flow graph
+-- node in control flow graph are int, which maps to an instruction
+-- transformation do two main thing:
+-- 1. add edge from instruction to instruction (node to node, int to int)
+-- 2. add mapping : node -> temps defined at the node (def)
+-- 3. add mapping : node -> temps used at the node    (use)
 instrsToGraph :: [Assem.Instr] -> FlowGraph
 instrsToGraph instrs = fGraph { nodes = map fst indexed,
                                 assems = HashMap.fromList indexed }
@@ -28,17 +33,23 @@ instrsToGraph instrs = fGraph { nodes = map fst indexed,
           instrsToGraph' rest $ fGraph { control = adjGraph', def = def', use = use' }
           where
             targets = map (\l -> labelTable ! l) (jump instr)
+
+            -- add edge from current node to jump targets
             adjGraph' =
-              foldl (\acc t -> overlay (edge index t) acc) (control fGraph) targets 
+              foldl (\acc t -> overlay (edge index t) acc) (control fGraph) targets
+              
+            -- add def and use temps
             def' = insertWith (++) index dst (def fGraph)
             use' = insertWith (++) index src (use fGraph)
 
         instrsToGraph' (curr@(index, ILABEL _ _):rest) fGraph =
           instrsToGraph' rest $ fGraph { control = adjGraph'}
           where adjGraph' = overlay (edge index (index+1)) (control fGraph)
+          -- add edge from current node to next instruction
 
         instrsToGraph' ((index, (IMOV _ (dst:_) (src:_))):rest) fGraph =
           instrsToGraph' rest $ fGraph { control = adjGraph',
                                          def = insertWith (++) index [dst] (def fGraph),
                                          use = insertWith (++) index [src] (use fGraph) }
-          where adjGraph' = overlay (edge index (index+1)) (control fGraph)
+          where -- add edge from current node to next instruction
+                adjGraph' = overlay (edge index (index+1)) (control fGraph)
