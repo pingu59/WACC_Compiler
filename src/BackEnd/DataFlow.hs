d6ed4cde43efb1484b9339d845297f45d05fcbc2
Nanfeng Liu <nl517@texel38.doc.ic.ac.uk>
2019-03-08 19:16:51 +0000

    implementing constant propagation

--- src/BackEnd/DataFlow.hs
+++ src/BackEnd/DataFlow.hs
@@ -18,6 +18,9 @@ import Data.List
 4. transform
 -}
 
+type ReachingDef = [(Int, ([Int], [Int]))]
+type PredTable = [(Int, [Int])]
+
 isBM :: Exp -> Bool
 isBM (MEM _ _) = True
 isBM (BINEXP _ _ _) = True
@@ -32,7 +35,7 @@ testGKFile file = do
         (qfrag, qs'') = runState (mapM quadStm userFrags) qs'
         (stms, s') = runState (transform qstm) qs''
         (userFrags', _) = runState (mapM transform qfrag) s'
-        (gkmain, gkstate) = runState (wrapGK stms) newDataState
+        (gkmain, gkstate) = runState (wrapGK stms) newDataState --[DataFlow]
         (gkuser) = evalState (mapM wrapGK userFrags') gkstate
     return $ genReachingDef (gkmain ++ concat gkuser)
 
@@ -46,6 +49,61 @@ testQuadFile file = do
         (stms, s') = runState (transform qstm) qs''
         (userFrags', _) = runState (mapM transform qfrag) s'
     return $ stms ++ userFrags
+
+-- constProp :: ([Stm],ReachingDef) -> ([Stm],ReachingDef)
+-- {-if n(move const to temp) reches f with no def of n in between-}
+-- --stm[n] uses t
+-- --in[n] = [n1, n2, n3...] has only one def of t then replace t in stm[n] with const
+-- constProp (stms@(s:rest), table) = (constProp' stms table 0 stms,
+-- constProp stmAndTable = stmAndTable
+--
+-- constProp' :: [Stm] -> ReachingDef -> Int -> [Stm] -> [Stm]
+-- constProp' stm table i newStm
+--   | i == length stm = stm
+--   | otherwise =
+--       where
+--         s = stm !! i
+--         used = getRegStm s
+--         inDef = fst $ snd (table !! i)
+--         numDefs = map (findConst stm  0 0) used --[(num, c)]
+--
+--
+--
+-- replaceStm :: Stm -> Temp.Temp -> Int -> Stm
+-- replaceStm (MOV (Temp t1) e) t c = MOV (Temp t1) (replaceExp t c e)
+-- replaceStm (MOV e (Temp t)) t c = MOV e (CONSTI c)
+-- replaceStm (CJUMP cond e1 e2 l1 l2) t c =
+--   CJUMP cond (replaceExp t c e1) (replaceExp t c e2) l1 l2
+-- replaceStm (EXP (CALL f exps)) t c = EXP (CALL f (map (replaceExp t c) exps))
+-- replaceStm stm _ _ = stm
+--
+-- replaceExp :: Temp.Temp -> Int -> Exp -> Exp
+-- replaceExp t c (Temp t) = CONSTI c
+-- replaceExp t c (BINEXP bop e1 e2) = BINEXP bop (replaceExp t c e1) (replaceExp t c e2)
+-- replaceExp (MEM e i) = MEM (replaceExp t c e) i
+-- replaceExp (CALL f exps) = CALL f (map (replaceExp t c) exps)
+-- replaceExp _ = []
+--
+--
+-- findConst :: [Stm] -> Int -> Int -> Temp.Temp -> (Int, Int)
+-- findConst [] num c t = (num, c) --number of definition of t with the latest one at pos with const i
+-- findConst ((MOV (TEMP t) (CONSTI i)):ds) num c t = findConst ds (num + 1) i t
+-- findConst (d:ds) num c t = findConst ds num c t
+--
+-- --get the list of temps used in one statement
+-- getRegStm :: Stm -> [Temp.Temp]
+-- getRegStm (MOV (Temp t) e) = getRegExp e
+-- getRegStm (MOV _ (Temp t)) = [t]
+-- getRegStm (CJUMP _ e1 e2 _ _) = concat $ map getRegExp [e1,e2]
+-- getRegStm (EXP (CALL _ exps)) = concat $ map getRegExp exps
+-- getRegStm _ = []
+--
+-- getRegExp :: Exp -> [Temp.Temp]
+-- getRegExp (Temp t) = [t]
+-- getRegExp (BINEXP _ e1 e2) = concat $ map getRegExp [e1,e2]
+-- getRegExp (MEM e _) = getRegExp e
+-- getRegExp (CALL _ exps) = concat $ map getRegExp exps
+-- getRegEXp _ = []
 
 eseq :: State TranslateState (Exp -> Exp)
 eseq = do
@@ -164,10 +222,10 @@ wrapOneGk m@(M (MOV (TEMP a) _) gen _ _) = getKill m a gen
 
 wrapOneGk x = return x
 
-genPred :: [DataFlow] -> [(Int, [Int])]
+genPred :: [DataFlow] -> PredTable
 genPred flow = genPred' flow flow []
 
-genPred' :: [DataFlow] -> [DataFlow] -> [(Int, [Int])] -> [(Int, [Int])]
+genPred' :: [DataFlow] -> [DataFlow] -> PredTable -> PredTable
 genPred' src [] acc = acc
 genPred' src ((E (LABEL l) deftid) : rest) acc = genPred' src rest (acc ++ [(deftid, (searchLable l))])
     where
@@ -192,8 +250,8 @@ genPred' src ((E _ deftid) : rest) acc = genPred' src rest (acc ++ [(deftid, (pr
 prevId 0 = []
 prevId x = [x - 1]
 
---              deftid  in          gkout                       init reachingdef
-initReachingDef :: [DataFlow] -> ([(Int, ([Int], [Int]))], ([(Int, ([Int], [Int]))]))
+--              deftid  in          gkout      init reachingdef
+initReachingDef :: [DataFlow] -> (ReachingDef, ReachingDef)
 initReachingDef flows = (map takeGK flows, map initReach flows)
     where
         takeGK (M tree g k deftid) = (deftid, (g, k))
@@ -201,14 +259,14 @@ initReachingDef flows = (map takeGK flows, map initReach flows)
         initReach (M _ _ _ deftid) = (deftid, ([], []))
         initReach (E tree deftid) = (deftid, ([], []))
 
---                    gk table                     current reaching def         pred table                 output reaching def
-iterateReachingDef :: [(Int, ([Int], [Int]))] -> [(Int, ([Int], [Int]))] -> [(Int, [Int])] -> [(Int, ([Int], [Int]))]
+--                    gk table       current reaching def   pred table  output reaching def
+iterateReachingDef :: ReachingDef -> ReachingDef -> PredTable -> ReachingDef
 iterateReachingDef gk this pred
   | this == next = this
   | otherwise = iterateReachingDef gk next pred
   where
     next = iterateOnce this []
-    iterateOnce :: [(Int, ([Int], [Int]))] -> [(Int, ([Int], [Int]))] ->  [(Int, ([Int], [Int]))]
+    iterateOnce :: ReachingDef -> ReachingDef ->  ReachingDef
     iterateOnce [] acc = acc
     iterateOnce ((deftid, (in_, out_)):remain) acc = iterateOnce remain (acc ++ [updated])
         where
@@ -219,7 +277,7 @@ iterateReachingDef gk this pred
             (gen, kill) = fromJust $ (Data.List.lookup) deftid gk
             newOut = union  gen (in_ \\ kill)
 
-genReachingDef :: [DataFlow] -> [(Int, ([Int], [Int]))]
+genReachingDef :: [DataFlow] -> ReachingDef
 genReachingDef flow = iterateReachingDef gk init pred
     where
         (gk, init) = initReachingDef flow
